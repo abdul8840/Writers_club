8da5de9b7a2d67e7b67515b04f92566312a898ad
Abdul-Rahman <abdul14941naseer@gmail.com>
2024-03-17 21:44:37 +0530

    created fully functional profile page with update and delete functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Signin from './pages/Signin';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
+import PrivateRoute from './components/PrivateRoute';
 
 
 const App = () => {
@@ -17,7 +18,9 @@ const App = () => {
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
+        <Route element={<PrivateRoute />}>
         <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
